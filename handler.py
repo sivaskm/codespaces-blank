72a68c29ee43fc2fea7a5ffb59269e63a0f5c63a
sivask <sksivaselvaraj2000@gmail.com>
2024-01-20 03:43:01 +0000

    fastapi services added for handling cv and jd.

--- handler.py
+++ handler.py
@@ -1,8 +1,29 @@
 from prompt import get_prompt
 from data import jd, resume
 from llm import get_resp_from_llm
+from pypdf import PdfReader
+from io import BytesIO
 
 def get_response(user_input):
     prompt = get_prompt(jd, resume)
     resp = get_resp_from_llm(prompt)
     return resp
+
+def extract_text_from_pdf(file):
+    file_content = file.read()
+    pdf_reader = PdfReader(BytesIO(file_content))
+    return pdf_reader.pages[0].extract_text()
+
+def save_text(text: str, file_name: str):
+    with open(f"data/{file_name}.txt", "w") as text_file:
+        text_file.write(text)
+
+def save_jd(text, file):
+    if file:
+        text = extract_text_from_pdf(file.file)
+    save_text(text, "jd")
+
+def save_cv(file):
+    if file:
+        text = extract_text_from_pdf(file.file)
+        save_text(text, "cv")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI 
 from schema import UserRequest
-from handler import get_response
+from handler import save_cv, save_jd, extract_text_from_pdf
+from fastapi import FastAPI, File, UploadFile, HTTPException, Form
+from typing import Dict
 
 app = FastAPI()
 
@@ -9,3 +11,15 @@ app = FastAPI()
 async def query(user_request: UserRequest):
     response = get_response(user_request.user_input)
     return response
+
+
+@app.post("/uploadjd/")
+async def upload_job_description(text: str = Form(None), file: UploadFile = File(None)):
+    # print(extract_text_from_pdf(file))
+    save_jd(text, file)
+
+@app.post("/uploadcv/")
+async def upload_job_description(file: UploadFile = File(None)):
+    save_cv(file)
+
+
